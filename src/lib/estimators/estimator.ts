a95050918270de4ab971283df026f2e2f8e697cc
Paul1365972 <paul1365972@gmail.com>
2025-03-25 15:25:43 +0100

    Disable formant estimator experiments

--- src/lib/estimators/estimator.ts
+++ src/lib/estimators/estimator.ts
@@ -3,10 +3,12 @@ import { MAX_HISTORY, type AudioManager } from '../audio'
 import { findFundamentalFrequency, findMaximumFrequencies } from './custom_estimator'
 import { type SpectrogramSettings } from '../settings'
 import { computeLPCCoefficients, computeLPCFormants } from './lpc'
+import { EfficientFormant } from './efficient_formant'
 
 export class EstimatorManager {
 	private results: EstimatorResult[] = []
 	private pitchDetector: PitchDetector<Float32Array> | null = null
+	private efficientFormant: EfficientFormant | null = null
 
 	constructor(private audioManager: AudioManager) {}
 
@@ -24,14 +26,24 @@ export class EstimatorManager {
 			findFundamentalFrequency(this.audioManager, audioBuffer, 8)
 		const frequencyMaximas = findMaximumFrequencies(this.audioManager, audioBuffer, 3)
 
-		if (!this.pitchDetector || this.pitchDetector.inputLength != timeBuffer.length) {
+		if (!this.pitchDetector || this.pitchDetector.inputLength !== timeBuffer.length) {
 			this.pitchDetector = PitchDetector.forFloat32Array(timeBuffer.length)
 		}
-		const [pitchyFrequency, pitchyConfidence] = this.pitchDetector.findPitch(timeBuffer, sampleRate)
+		const [pitchyFrequency, pitchyConfidence] = this.pitchDetector.findPitch(
+			timeBuffer.slice(),
+			sampleRate,
+		)
 
-		// const lpcCoefficients = computeLPCCoefficients(timeBuffer, 10)
-		const lpcCoefficients = new Float32Array([1.0])
+		// if (!this.efficientFormant || this.efficientFormant.sampleRate !== sampleRate) {
+		// 	 this.efficientFormant = new EfficientFormant(sampleRate)
+		// }
+		// const formants = this.efficientFormant.estimate(timeBuffer.slice(), 4)
+		// console.log(formants)
+
+		// const lpcCoefficients = computeLPCCoefficients(timeBuffer.slice(), sampleRate, 8)
 		// const formants = computeLPCFormants(lpcCoefficients, sampleRate)
+		// console.log(formants)
+		const lpcCoefficients = new Float32Array()
 
 		if (this.results.length >= MAX_HISTORY) {
 			this.results.pop()
